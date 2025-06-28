b500af913a81bec5dc4373e112d4eb3c3705582b
AbhihShrey <abhih.shrey@gmail.com>
2025-06-27 18:36:16 -0700

    changed the port to 10000 for render

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ def chat():
     return backend.handle_chat(request)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
